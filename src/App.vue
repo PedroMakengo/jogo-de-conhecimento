7909b8372e6a95926e0563acf648d4bb0d26ab70
PedroMakengo <pedromakengomiguel@gmail.com>
2022-10-06 09:06:15 +0000

    Trabalhando com requisição http

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,21 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
+  <div>
+    <h1>Our first vue js app</h1>
+
+    <input type="radio" name="options" value="True" />
+    <label for="">True</label> <br />
+    <input type="radio" name="options" value="False" />
+    <label for="">False</label><br />
+
+    <button class="send" type="button">Send</button>
+  </div>
 </template>
 
 <script>
 export default {
   name: "App",
 };
+// https://opentdb.com/api.php?amount=1&category=18
 </script>
 
 <style lang="scss">
@@ -15,6 +25,23 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 60px auto;
+
+  max-width: 960px;
+
+  input[type="radio"] {
+    margin: 12px 4px;
+  }
+
+  button.send {
+    margin-top: 12px;
+    height: 40px;
+    min-width: 120px;
+    padding: 0 16px;
+    color: #fff;
+    background: #1867c0;
+    border: 1px solid #1867c0;
+    cursor: pointer;
+  }
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import axios from 'axios'
-import VueAxios from 'vue-axios'
+import { createApp } from "vue";
+import App from "./App.vue";
+import axios from "axios";
+import VueAxios from "vue-axios";
 
-createApp(App).mount('#app')
+createApp(App).mount("#app").use(VueAxios, axios);
